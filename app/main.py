b685f0d4a6127ab6b2d0afd670f7f55cf3c12e5a
Nikhil18Patil <nikhilpatil18012004@gmail.com>
2025-04-10 01:44:24 +0530

    vercel dependency added

--- app/main.py
+++ app/main.py
@@ -1,23 +1,23 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+print("🚀 App is starting...")
+
 from fastapi import FastAPI
-from app.database import Base, engine
-from app.routers import url
+from database import Base, engine
+from routers import url
 from mangum import Mangum
 
-# Create tables
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Clean URL Shortener")
 
-# Include the URL shortener routes
 app.include_router(url.router)
-
 
 @app.get("/")
 def read_root():
     return {
         "message": "👋 Hey there! Welcome to the Clean URL Shortener API.",
-        f"usage": "Use {http://127.0.0.1:8000/docs} for Swagger UI or {http://127.0.0.1:8000/redoc} for ReDoc to explore and test the API."
+        "usage": "Visit /docs for Swagger UI or /redoc for ReDoc to explore and test the API."
     }
 
-# Required for Vercel (AWS Lambda-style handler)
 handler = Mangum(app)
